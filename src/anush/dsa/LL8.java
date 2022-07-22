87e36fcd0977f16a680634b05c9db6b1ad7f4437
Anush-Krish <107431883+Anush-Krish@users.noreply.github.com>
2022-07-22 12:22:02 +0530

    LL8

--- src/anush/dsa/LL8.java
+++ src/anush/dsa/LL8.java
@@ -2,5 +2,55 @@ package anush.dsa;
 //palindrome linked list.
 public class LL8 {
     Node head;
-    
+    public void addLast(int data){
+        Node newNode=new Node(data);
+        Node currNode=head;
+        if(head==null){
+            head=newNode;
+        }
+        while(currNode.next!=null){
+            currNode=currNode.next;
+        }
+        currNode.next=newNode;
+    }
+    public void printList(){
+        if(head==null){
+            System.out.println("List is Empty.");
+        }
+        Node currNode=head;
+        while(currNode!=null){
+            currNode=currNode.next;
+        }
+    }
+    public int getSize(){
+        int size=0;
+        if(head==null){
+            return size;
+        }
+        Node currNode=head;
+        while(currNode!=null){
+            size++;
+            currNode=currNode.next;
+
+        }
+        return size;
+    }
+    public void isPalindrome(){
+        if(head==null){
+            System.out.println("List is Empty");
+        }
+        if(head.next==null){
+            System.out.println("Palindrome");
+        }
+
+    }
+    public void main(String[] args){
+        LL8 list = new LL8();
+        list.addLast(1);
+        list.addLast(2);
+        list.addLast(3);
+        list.addLast(4);
+        list.addLast(5);
+        list.printList();
+    }
 }
