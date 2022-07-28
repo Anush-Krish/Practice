b0f19f95128d7532678379100623242ca276ab4e
Anush-Krish <107431883+Anush-Krish@users.noreply.github.com>
2022-07-29 00:31:50 +0530

    LL8

--- src/anush/dsa/LL8.java
+++ src/anush/dsa/LL8.java
@@ -1,56 +1,112 @@
 package anush.dsa;
+
 //palindrome linked list.
 public class LL8 {
     Node head;
-    public void addLast(int data){
-        Node newNode=new Node(data);
-        Node currNode=head;
-        if(head==null){
-            head=newNode;
+
+    public void addLast(int data) {
+        Node newNode = new Node(data);
+
+        if (head == null) {
+            head = newNode;
+            return;
         }
-        while(currNode.next!=null){
-            currNode=currNode.next;
+        Node currNode = head;
+        while (currNode.next != null) {
+            currNode = currNode.next;
         }
-        currNode.next=newNode;
+        currNode.next = newNode;
     }
-    public void printList(){
-        if(head==null){
+
+    public void printList() {
+        if (head == null) {
             System.out.println("List is Empty.");
         }
-        Node currNode=head;
-        while(currNode!=null){
-            currNode=currNode.next;
+        Node currNode = head;
+        while (currNode != null) {
+            System.out.print(currNode.data + "-->");
+            currNode = currNode.next;
         }
+        System.out.println("NULL");
     }
-    public int getSize(){
-        int size=0;
-        if(head==null){
+
+    public int getSize() {
+        int size = 0;
+        if (head == null) {
             return size;
         }
-        Node currNode=head;
-        while(currNode!=null){
+        Node currNode = head;
+        while (currNode != null) {
             size++;
-            currNode=currNode.next;
+            currNode = currNode.next;
 
         }
         return size;
     }
-    public void isPalindrome(){
-        if(head==null){
-            System.out.println("List is Empty");
+
+
+
+    public Node findMiddle(Node head) {
+        Node hare = head;
+        Node turtle = head;
+
+        while (hare.next != null && hare.next.next != null) {
+            hare = hare.next.next;
+            turtle = turtle.next;
         }
-        if(head.next==null){
-            System.out.println("Palindrome");
+        return turtle;
+    }
+
+    public Node reverse(Node head) {
+
+        Node prevNode = null;
+        Node currNode = head;
+        while (currNode != null) {
+            Node nextNode = currNode.next;
+            currNode.next = prevNode;
+            prevNode = currNode;
+            currNode = nextNode;
+
         }
 
+        return prevNode;
     }
-    public void main(String[] args){
+
+    public boolean isPalindrome(Node head) {
+        if (head == null || head.next == null) {
+            return true;
+        }
+        Node middle = findMiddle(head);
+        Node secondStart = reverse(middle.next);
+        Node firstStart = head;
+        while (secondStart != null) {
+            if (firstStart.data != secondStart.data) {
+                return false;
+            }
+
+            firstStart = firstStart.next;
+            secondStart = secondStart.next;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
         LL8 list = new LL8();
         list.addLast(1);
         list.addLast(2);
-        list.addLast(3);
-        list.addLast(4);
-        list.addLast(5);
+        list.addLast(2);
+        list.addLast(2);
+        list.addLast(1);
+        //list.addLast(5);
         list.printList();
+        boolean flag = list.isPalindrome(list.head);
+        if (flag == true) {
+            System.out.println("palindrome");
+
+
+        } else {
+            System.out.println("Not a palindrome");
+        }
     }
 }
+
---
+++ src/anush/dsa/LL9.java
@@ -0,0 +1,4 @@
+package anush.dsa;
+
+public class LL9 {
+}
