3a9d8b2d25321a04e2261f479396d2d12031975a
Anush-Krish <107431883+Anush-Krish@users.noreply.github.com>
2025-01-08 23:13:39 +0530

    find and delete node

--- src/anush/revisitdsa/LinkedListOne.java
+++ src/anush/revisitdsa/LinkedListOne.java
@@ -37,40 +37,52 @@ public class LinkedListOne {
     }
 
     private void findAndDeleteElement(int data) {
-        Node temp = head;
-        while (null != temp) {
+        Node curr = head.next;
+        Node prev = head;
+
+        while (null != prev) {
             //if deletion data is found
-            if (temp.data == data) {
-                if (null == temp.next) { //if one element exist
+            if (prev.data == data) {
+                if (null == head.next) {       //if one element exist
                     head = null;
+                    break;
+                } else if (prev == head) {     // if the deletion element is head
+                    head = curr;
+                    break;
                 }
-                if (temp == head) {     // if the deletion element is head
-                    temp.next=head;
-                }
-                if(temp )
-                temp = temp.next;
-
+            } else if (curr.data == data) {
+                //if any middle element
+                prev.next = curr.next;
             }
-        }
+            prev = prev.next;
+            curr = curr.next;
 
-        private void printList () {
-            if (null == head) {
-                System.out.println("Empty list");
-                return;
-            }
-            Node temp = head;
-            while (null != temp) {
-                System.out.print(temp.data + "-->");
-                temp = temp.next;
-            }
-            System.out.println("null");
-        }
-
-        public static void main (String[]args){
-            LinkedListOne linkedList = new LinkedListOne();
-            linkedList.addFirst(10);
-            linkedList.addFirst(20);
-            linkedList.addLast(30);
-            linkedList.printList();
         }
     }
+
+    private void printList() {
+        if (null == head) {
+            System.out.println("Empty list");
+            return;
+        }
+        Node temp = head;
+        while (null != temp) {
+            System.out.print(temp.data + "-->");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+
+    public static void main(String[] args) {
+        LinkedListOne linkedList = new LinkedListOne();
+        linkedList.addFirst(10);
+        linkedList.addFirst(20);
+        linkedList.addFirst(30);
+        linkedList.addFirst(40);
+        linkedList.addLast(50);
+        linkedList.printList();
+        linkedList.findAndDeleteElement(50);
+        linkedList.printList();
+
+    }
+}
